99ce0438e29e75c2b56a1f37870e7a144ef57be5
Lloyd Smith <lloyd.smith@gmail.com>
2010-06-24 18:08:58 -0400

    Unit test for the custom tag! Test coverage is becoming pretty awesome. (Okay - now I'm really committing the test)

---
+++ src/test/java/com/upthescala/viewprotect/ViewProtectTagTest.java
@@ -0,0 +1,101 @@
+package com.upthescala.viewprotect;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.Tag;
+
+import org.acegisecurity.Authentication;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.springframework.mock.web.MockPageContext;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.upthescala.viewprotect.tag.ViewProtectTag;
+
+import static com.upthescala.viewprotect.basic.BasicTestSupport.array;
+import static org.testng.Assert.assertEquals;
+
+import static org.easymock.EasyMock.*;
+
+public class ViewProtectTagTest {
+
+	private ViewProtectTag tag;
+	private MockPageContext pageContext;
+	private MockServletContext servletContext;
+
+	private Authentication user;
+	private SecurityContext securityContext;
+	private ViewAuthorizationService viewAuthorizationService;
+
+	private Object[] mocks;
+	private WebApplicationContext applicationContext;
+
+	@BeforeMethod
+	public void setUp() {
+		tag = new ViewProtectTag();
+
+		servletContext = new MockServletContext();
+
+		applicationContext = createMock("applicationContext",
+				WebApplicationContext.class);
+
+		servletContext.setAttribute(
+				WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE,
+				applicationContext);
+		pageContext = new MockPageContext(servletContext);
+
+		tag.setPageContext(pageContext);
+
+		user = createMock("userAuthentication", Authentication.class);
+		securityContext = createMock("securityContext", SecurityContext.class);
+		viewAuthorizationService = createMock("viewAuthorizationService",
+				ViewAuthorizationService.class);
+
+		mocks = array(user, securityContext, viewAuthorizationService,
+				applicationContext);
+
+		reset(mocks);
+
+		SecurityContextHolder.setContext(securityContext);
+	}
+
+	@DataProvider(name = "protectTag")
+	public Object[][] protectTagDataProvider() {
+		return new Object[][] { { "foo", false, Tag.SKIP_BODY },
+				{ "foo", true, Tag.EVAL_BODY_INCLUDE }, };
+	}
+
+	@Test(dataProvider = "protectTag")
+	public void testProtectTag(final String componentId,
+			final boolean isAuthorized, final int expectedDoStartTagResult)
+			throws JspException {
+
+		expect(
+				applicationContext.getBean("viewAuthorizationService",
+						ViewAuthorizationService.class)).andReturn(
+				viewAuthorizationService);
+
+		expect(securityContext.getAuthentication()).andReturn(user);
+		expect(viewAuthorizationService.isAuthorizedForUser(componentId, user))
+				.andReturn(isAuthorized);
+
+		replay(mocks);
+
+		tag.setComponentId(componentId);
+
+		assertEquals(tag.doStartTag(), expectedDoStartTagResult,
+				"doStartTagResult does not match expected");
+
+		verify(mocks);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		SecurityContextHolder.clearContext();
+
+	}
+}
