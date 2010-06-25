449b57290b4f0a29b57f18215333d66ea382e250
Lloyd Smith <lloyd.smith@gmail.com>
2010-06-25 10:49:02 -0400

    Javadoc for methods in BasicTestSupport (the purpose of this project is as much to be a learning tool as useful software)

--- src/test/java/com/upthescala/viewprotect/basic/BasicTestSupport.java
+++ src/test/java/com/upthescala/viewprotect/basic/BasicTestSupport.java
@@ -2,37 +2,98 @@ package com.upthescala.viewprotect.basic;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.GrantedAuthorityImpl;
+import org.testng.annotations.DataProvider;
 
 /**
- * A container for helper methods us
+ * A number of "fluently-named" convenience methods for use in unit tests. These
+ * methods should be combined with static imports in a unit test class to
+ * produce more readable code. This methods are especially useful for defining
+ * the tuples returned by {@link DataProvider} methods.
+ * 
+ * Example:
+ * 
+ * <pre>
+ * &#64;DataProvider(name="roleTestProvider")
+ * public Object[][] roleTestDataProvider() {
+ *    // &#64formatter:off
+ *    return new Object[][] {
+ *       {allowed("ROLE_A"),denied("ROLE_C"), basicAttributes(notGranted("FOO"),null,anyGranted("ROLE_X","ROLE_Y")},
+ *       ...
+ *    };
+ *    // &#64formatter:on
+ * }
+ * 
+ * &#64;Test(dataProvider="roleTestProvider")
+ * public void roleTest(Set<String> allowedRoles, Set<String> deniedRoles, BasicComponentAttributes attrs) {
+ *     ...
+ * }
+ * </pre>
+ * 
+ * As a result of using fluently-named helper methods such as those defined in
+ * this class, the data-driven tests become much more readable.
+ * 
+ * @author Lloyd Smith II
  */
 public class BasicTestSupport {
 
+	/**
+	 * @param <T>
+	 *            the type of the elements
+	 * @param items
+	 *            the elements to add to the HashSet
+	 * @return a new HashSet containing the {@code items}
+	 */
 	public static <T> Set<T> setOf(final T... items) {
-		if (items.length == 0)
-			return Collections.emptySet();
 		return new HashSet<T>(Arrays.asList(items));
 	}
 
-	public static AnyGrantedAttribute anyGranted(final String... items) {
-		return new AnyGrantedAttribute(setOf(items));
+	/**
+	 * @param roles
+	 *            the roles
+	 * @return a new AnyGrantedAtribute with a decision set initialized to the
+	 *         specified roles
+	 */
+	public static AnyGrantedAttribute anyGranted(final String... roles) {
+		return new AnyGrantedAttribute(setOf(roles));
 	}
 
-	public static AllGrantedAttribute allGranted(final String... items) {
-		return new AllGrantedAttribute(setOf(items));
+	/**
+	 * 
+	 * @param roles
+	 *            the roles
+	 * @return a new AllGrantedAttribute with a decision set initialized to the
+	 *         specified roles
+	 */
+	public static AllGrantedAttribute allGranted(final String... roles) {
+		return new AllGrantedAttribute(setOf(roles));
+	}
+	/**
+	 * 
+	 * @param roles
+	 *            the roles
+	 * @return a new NotGrantedAttribute with a decision set initialized to the
+	 *         specified roles
+	 */
+	public static NotGrantedAttribute notGranted(final String... roles) {
+		return new NotGrantedAttribute(setOf(roles));
 	}
 
-	public static NotGrantedAttribute notGranted(final String... items) {
-		return new NotGrantedAttribute(setOf(items));
-	}
-
+	/**
+	 * 
+	 * @param notGranted
+	 *            a NotGrantedAttribute (possibly {@code null})
+	 * @param allGranted
+	 *            a AllGrantedAttribute (possibly {@code null})
+	 * @param anyGranted
+	 *            a AnyGrantedAttribute (possibly {@code null})
+	 * @return a new BasicComponentAttributes initialized with the arguments
+	 */
 	public static BasicComponentAttributes basicAttributes(
 			final NotGrantedAttribute notGranted,
 			final AllGrantedAttribute allGranted,
@@ -44,22 +105,62 @@ public class BasicTestSupport {
 		return basicComponentAttributes;
 	}
 
+	/**
+	 * 
+	 * @param <T>
+	 * @param items
+	 * @return a new HashSet of items fluently thought of as being "allowed"
+	 *         (e.g., allowed user roles)
+	 */
 	public static <T> Set<T> allowed(final T... items) {
 		return setOf(items);
 	}
-
+	/**
+	 * 
+	 * @param <T>
+	 * @param items
+	 * @return a new HashSet of items fluently thought of as being "denied"
+	 *         (e.g., denied user roles)
+	 */
 	public static <T> Set<T> denied(final T... items) {
 		return setOf(items);
 	}
-
+	/**
+	 * @param <T>
+	 * @param roles
+	 *            the roles
+	 * @return a new String array of roles fluently thought of as being
+	 *         "granted roles"
+	 */
 	public static String[] grantedRoles(final String... roles) {
 		return array(roles);
 	}
 
+	/**
+	 * A convenience method to create an array of elements with less syntax that
+	 * new Object[]{ one, two, ..., last }.
+	 * 
+	 * Example:
+	 * 
+	 * <pre>
+	 * doSomethingWithArray(array("thing1", "thing2", thing3"));
+	 * </pre>
+	 * 
+	 * @param <T>
+	 * @param items
+	 *            the elements of the array
+	 * @return an array of the items
+	 */
 	public static <T> T[] array(final T... items) {
 		return items;
 	}
 
+	/**
+	 * @param roles
+	 *            the roles
+	 * @return an array containing a {@code GrantedAuthority} for each role;
+	 *         {@link GrantedAuthorityImpl} is used
+	 */
 	public static GrantedAuthority[] auths(final String... roles) {
 		List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>(
 				roles.length);
@@ -69,8 +170,13 @@ public class BasicTestSupport {
 		return auths.toArray(new GrantedAuthority[roles.length]);
 	}
 
+	/**
+	 * @param roles
+	 *            the roles to add to the set
+	 * @return a new set consisting of the specified roles
+	 */
 	public static Set<String> roles(final String... roles) {
-		return new HashSet<String>(Arrays.asList(roles));
+		return setOf(roles);
 	}
 
 }
